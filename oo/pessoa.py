b4338eefbf52154f9b1b5a0b0b863c1924e02cdd
Leonardo Cotta <leonardocottad@gmailcom>
2021-11-08 16:08:26 -0300

    Criando criando classe Homem

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -17,9 +17,12 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - {cls.olhos}'
 
+class Homem(Pessoa):
+    pass
+
 if __name__ == '__main__':
-    phelipe = Pessoa(nome='Phelipe')
-    carlos = Pessoa(phelipe, nome='Carlos')
+    phelipe = Homem(nome='Phelipe')
+    carlos = Homem(phelipe, nome='Carlos')
     print(Pessoa.cumprimentar(phelipe))
     print(id(phelipe))
     print(phelipe.cumprimentar())
@@ -41,3 +44,8 @@ if __name__ == '__main__':
     print(id(Pessoa.olhos), id(carlos.olhos), id(phelipe.olhos))
     print(Pessoa.metodo_estatico(), carlos.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), carlos.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(phelipe, Pessoa))
+    print(isinstance(phelipe, Homem))
